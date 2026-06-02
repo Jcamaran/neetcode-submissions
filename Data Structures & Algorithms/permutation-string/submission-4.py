49449c901078a897a23a8a1a1c515422df95c3d2
Joaquin C <128087254+Jcamaran@users.noreply.github.com>
2026-06-02 16:18:14 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,37 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+            
+        s1_count,s2_count = {},{}
+
+        for i in range(len(s1)):
+            # for every character in len of s1 we add the character and its count to the hashtable
+            s1_count[s1[i]] = 1 + s1_count.get(s1[i],0)
+            s2_count[s2[i]] = 1 + s2_count.get(s2[i],0)
+        
+        if s1_count == s2_count:
+            return True
+        
+        left = 0
+
+        for right in range(len(s1),len(s2)):
+            # now iterating through the remaining elements of s2 we add the right element and remove 1 from left with sliding window approach
+            s2_count[s2[right]] = 1 + s2_count.get(s2[right],0)
+            s2_count[s2[left]] -= 1
+
+            if s2_count[s2[left]] == 0:
+                del s2_count[s2[left]]
+            
+            left += 1
+
+            if s1_count == s2_count:
+                return True
+
+        return False
+
+
+
+
+
+        
