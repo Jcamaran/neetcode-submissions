e29f917b0efd4bc701eca413805cad2155064a5f
Joaquin C <128087254+Jcamaran@users.noreply.github.com>
2026-07-23 20:36:39 -0400

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # immedietly i think of a set to not include repearing characters
+        # a sliding window to find said substring
+
+        setS = set()
+
+        l = res = 0
+
+        for r in range(len(s)):
+            while s[r] in setS:
+                setS.remove(s[l])
+                l += 1
+            
+            setS.add(s[r])
+            res = max(res, r-l+1)
+            
+
+          
+        return res
+
+        
